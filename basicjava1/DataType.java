7630852cdaa68a3a2e0cdbf4d4222276cb1b13fe
arifabulhossain <arifabulhossain@gmail.com>
2018-12-18 10:14:49 +0600

    add

---
+++ basicjava1/DataType.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package basicjava1;
+
+/**
+ *
+ * @author User
+ */
+public class DataType {
+    public static void main(String[] args) {
+        boolean b = true;
+        int i = 25543  ;
+        char c = 'x';
+        float f = 100f;
+        double d = 20;
+        byte k = 10;
+        System.out.println("double = "+d);
+        System.out.println("b = " +b);
+        System.out.println("f = " +f);
+        System.out.println("c =  " + c);
+        System.out.println( "integer = "+i);
+         System.out.println( "The Result is = "+k);
+        
+        
+        
+        
+    }
+}
