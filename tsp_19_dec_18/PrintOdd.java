afd02374939a54c09a4585a2498b6d4e6136f8fb
Arif Abul Hossain <arifabualhossain@gmail.com>
2018-12-19 18:24:03 +0600

    odd/print/resultsheet/scanner

---
+++ tsp_19_dec_18/PrintOdd.java
@@ -0,0 +1,15 @@
+/*
+ Ques. 03. Print  odd number from 1 to 20;
+ */
+package tsp_19_dec_18;
+
+
+public class PrintOdd {
+    public static void main(String[] args) {
+       int i =1;
+       for (i =1 ; i <=20 ; i = i+2) {
+           System.out.println("The odd number is :"+i);
+       
+       }
+    }
+}
