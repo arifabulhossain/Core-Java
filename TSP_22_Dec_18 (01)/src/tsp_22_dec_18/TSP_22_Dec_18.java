6b7db216e21bd52fbfa39c46785ae85e24f69346
Arif Abul Hossain <arifabualhossain@gmail.com>
2018-12-22 18:46:46 +0600

    tsp

---
+++ TSP_22_Dec_18 (01)/src/tsp_22_dec_18/TSP_22_Dec_18.java
@@ -0,0 +1,28 @@
+/*
+ 
+ */
+package tsp_22_dec_18;
+
+
+public class TSP_22_Dec_18 {
+
+   
+    public static void main(String[] args) {
+      int x = 10;
+      x++;
+      --x;
+      x+=5;
+      ++x;
+        System.out.println("X:"+x);
+       
+       String a1 = "Abul";
+       String a2 = " Hossain Arif";
+        System.out.println(a1.concat(a2));
+        
+ Math.toDegrees(Math.PI/2);
+        
+        
+       
+    }
+    
+}
---
+++ TSP_22_Dec_18 (01)/src/tsp_22_dec_18/TrigonometicMethods.java
@@ -0,0 +1,18 @@
+/*
+
+ */
+package tsp_22_dec_18;
+
+public class TrigonometicMethods {
+
+    public static void main(String[] args) {
+        double x = Math.toDegrees(Math.PI / 2);
+        Math.toDegrees(Math.PI / 2);
+
+        System.out.println(x);
+        double s = Math.sin(Math.PI / 2);
+        Math.sin(Math.PI / 2);
+        System.out.println(s);
+       
+    }
+}
