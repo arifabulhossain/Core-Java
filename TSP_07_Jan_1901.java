8fff8d6d3077fdad061c1ff2851ec11e9f3f1125
Arif Abul Hossain <arifabualhossain@gmail.com>
2019-01-08 15:16:23 +0600

    tsp

---
+++ TSP_07_Jan_1901.java
@@ -0,0 +1,34 @@
+/*
+ 
+ */
+package tsp_07_jan_19.pkg01;
+
+
+public class TSP_07_Jan_1901 {
+
+    public static void main(String[] args) {
+        char[][] answers = {
+      {'A', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+      {'D', 'B', 'A', 'B', 'C', 'A', 'E', 'E', 'A', 'D'},
+      {'E', 'D', 'D', 'A', 'C', 'B', 'E', 'E', 'A', 'D'},
+      {'C', 'B', 'A', 'E', 'D', 'C', 'E', 'E', 'A', 'D'},
+      {'A', 'B', 'D', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+      {'B', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+      {'B', 'B', 'A', 'C', 'C', 'D', 'E', 'E', 'A', 'D'},
+      {'E', 'B', 'E', 'C', 'C', 'D', 'E', 'E', 'A', 'D'}};
+        
+        char[] keys = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
+         for (int i = 0; i < answers.length; i++) {
+      // Grade one student
+      int correctCount = 0;
+      for (int j = 0; j < answers[i].length; j++) {
+        if (answers[i][j] == keys[j])
+          correctCount++;
+      }
+
+      System.out.println("Student " + i + "'s correct count is " +
+        correctCount);
+    }
+    }
+    
+}
