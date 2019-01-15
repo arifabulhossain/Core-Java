b042a9a5eea58d3a6b78eeaec1f62d45d67ac509
arifabulhossain <arifabulhossain@gmail.com>
2019-01-15 08:13:09 +0600

    methpds

---
+++ Home_15_jan_19(01)/Method1/Test1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Method1;
+
+import static Method1.ReturnMethods.myMethods;
+
+/**
+ *
+ * @author User
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println(myMethods(5));
+    }
+ 
+}
---
+++ Home_15_jan_19(01)/Method2/Test2.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Method2;
+
+import static Method2.MethodsPractise.myMethods;
+
+/**
+ *
+ * @author User
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("The Result is = "+myMethods(20,30));
+    }
+
+ 
+}
