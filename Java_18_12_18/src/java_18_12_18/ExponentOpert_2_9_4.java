1ef7e5c01ab5d279fc4f96c5e59214c5018d0ba5
Arif Abul Hossain <arifabualhossain@gmail.com>
2019-01-18 18:56:53 +0600

    01

---
+++ Java_18_12_18/src/java_18_12_18/ExponentOpert_2_9_4.java
@@ -0,0 +1,16 @@
+/*
+ .
+ */
+package java_18_12_18;
+
+
+public class ExponentOpert_2_9_4 {
+
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.pow(4,0.5));
+        System.out.println(Math.pow(2.5,2));
+        System.out.println(Math.pow(2.5,-2));
+    }
+    
+}
