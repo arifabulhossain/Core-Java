90446fda3c035ba7d0db06bf781be4849e026813
arifabulhossain <arifabulhossain@gmail.com>
2019-02-02 02:30:34 +0600

    correction

--- Evidence Final/src/practise02/PrintPrime.java
+++ Evidence Final/src/practise02/PrintPrime.java
@@ -11,32 +11,32 @@ import java.util.Scanner;
 public class PrintPrime {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-      int i =0;
-      int num =0;
-      //Empty String
-      String  primeNumbers = "";
-      System.out.println("Enter the value of n:");
-      int n = scanner.nextInt();
-      scanner.close();
-      for (i = 1; i <= n; i++)  	   
-      { 		 		  
-         int counter=0; 		  
-         for(num =i; num>=1; num--)
-         {
-	    if(i%num==0)
-	    {
-		counter = counter + 1;
-	    }
-	 }
-	 if (counter ==2)
-	 {
-	    //Appended the Prime number to the String
-	    primeNumbers = primeNumbers + i + " ";
-	 }	
-      }	
-      System.out.println("Prime numbers from 1 to n are :");
-      System.out.println(primeNumbers);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the starting Number");
+        int sn = input.nextInt();
+        System.out.println("Enter the ending Number");
+        int en = input.nextInt();
+        int count = 0;
+         int total = 0;
+
+        for (int i = sn; i <= en; i++) {
+            if (i == 0 || i == 1) {
+                continue;
+            }
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    count++;
+                    break;
+                }
+            }
+            if (count == 0) {
+                System.out.println(i);
+                total++;
+            }
+            count = 0;
+
+        }
+        System.out.println("Total primes:" + total);
     }
 
 }
